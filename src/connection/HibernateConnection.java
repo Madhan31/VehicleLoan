6cdf056cc2d11ec42b467720f1e83916df3ce5ca
Madhan <madhan@ideas2it.com>
2016-09-07 14:39:48 +0530

    hibernate connection

--- src/connection/HibernateConnection.java
+++ src/connection/HibernateConnection.java
@@ -1,37 +1,60 @@
 package connection;
 
-import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+/**
+ * <p>
+ * Hibernate Connection is used to connect the application with database using Hibernate configuration
+ * Class is architectured using singleton factory concept.
+ * </p>
+ *
+ * @author Praveen RaJ
+ *
+ * @created 2016-09-01
+ */
 
 public class HibernateConnection {
-    private SessionFactory sessionFactory = null;
-    private AnnotationConfiguration configuration = null;
-    private static HibernateConnection hibernateConnection = null;
+   
+	private static HibernateConnection hibernateConnection = null;
+    private AnnotationConfiguration configuration=null;
+    private SessionFactory sessionFactory=null;
     
-    private HibernateConnection() {
-    }
+    /**
+     * Restricts object creation for this class
+     */
+    private HibernateConnection(){  
+    } 
     
-    public static HibernateConnection getInstance() {
-        if (hibernateConnection == null) {
+    /**
+     * This method is used to create object for this class
+     * It doesn't allow to create more then one object
+     * and returns the existing object.
+     * @return hibernateconnection
+     *        Contains object for class HibernateConnection
+     */
+    public static HibernateConnection createObject() {
+        if(hibernateConnection==null){
             hibernateConnection = new HibernateConnection();
-        }
+        }         
         return hibernateConnection;
     }
     
-    public SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            sessionFactory =  configuration.buildSessionFactory();
-        }
+    /**
+     * This Method is used to create object for sessionFactory through "hibernate.cfg.xml" file    
+     * It doesn't allow to create more then one object
+     * and returns the existing object.     
+     * @return sessionFactory
+     *        Contains object for class SessionFactory
+     */
+    public SessionFactory establishConnection() {
+        if(configuration==null){
+            configuration=new AnnotationConfiguration();
+   	        configuration.configure("hibernate.cfg.xml"); 
+   	    }
+   	    if(sessionFactory==null){	
+	        sessionFactory=configuration.configure().buildSessionFactory();
+	    }
         return sessionFactory;
-    }
-    
-    public AnnotationConfiguration getConfiguration() {
-        if (configuration == null) {
-            configuration = new AnnotationConfiguration();
-            configuration.configure("hibernate.cfg.xml");
-        }
-        return configuration;
-    }
-}
-    
+   }
+ }
