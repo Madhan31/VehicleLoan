41ff5ab0d8452f9217f9c080b1ca29e50793b9b9
viki148 <vigguvignesh407@gmail.com>
2016-09-09 20:58:53 +0530

    loanDetail and payment

--- src/service/PaymentService.java
+++ src/service/PaymentService.java
@@ -1,5 +1,6 @@
 package service;
 
+import model.Loan;
 import model.Payment;
 
 import dao.PaymentDao;
@@ -7,9 +8,16 @@ import exception.ApplicationException;
 
 public class PaymentService {
 	private PaymentDao paymentDao = new PaymentDao();
+	private LoanService loanService = new LoanService();
 	
 	public String addPayment(Payment payment) throws ApplicationException {
         paymentDao.addPayment(payment);
+        Loan loan = loanService.retrieveLoan(payment.getLoan().getLoanId());
+        if(loanDetailService.retrieveLoanDetail(payment.getLoan().getLoanId())) {
+           int balancePeriod = loan.getLoanPeriod();
+           int balanceAmount = loan.getLoanAmount();
+           LoanDetail loandetail = new LoanDetail(balanceAmount, balanceEmi);
+        }
         return "Successfully paided";
     }
 	
